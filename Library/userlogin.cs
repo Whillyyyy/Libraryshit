4004c28bfbe924cd95cc40f89d926efc2058b756
lexie <lexie@LAPTOP-500GRI3A>
2025-03-05 09:59:54 +0800

    new

--- Library/userlogin.cs
+++ Library/userlogin.cs
@@ -64,41 +64,52 @@ namespace Library
             {
                 MessageBox.Show("Please input Username and Password", "Error");
             }
-
             else
             {
-                connection.Open();
-                string selectQuery = "SELECT * FROM user_register WHERE studentnum = '" + studentnumtxt.Text + "' AND password = '" + passstudentxt.Text + "';";
-                MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
-                cmd.ExecuteNonQuery();
-                if (mdr.Read())
+                try
                 {
-                    string MyConnection2 = "Server=127.0.0.1;Database=library;username=root;password=";
-                    string Query = "update user_register set LastLogin='" + studentnumtxt.Text + "';";
-                    MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+                    // Open connection
+                    connection.Open();
 
-                    MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
-                    MySqlDataReader MyReader2;
-                    MyConn2.Open();
-                    MyReader2 = MyCommand2.ExecuteReader();
-                    while (MyReader2.Read())
+                    // Use parameterized query to prevent SQL injection
+                    string selectQuery = "SELECT * FROM user_register WHERE student_num = @studentnum AND passWord = @password";
+                    MySqlCommand cmd = new MySqlCommand(selectQuery, connection);
+                    cmd.Parameters.AddWithValue("@studentnum", studentnumtxt.Text);
+                    cmd.Parameters.AddWithValue("@password", passstudentxt.Text);
+
+                    MySqlDataReader mdr = cmd.ExecuteReader();
+
+                    if (mdr.HasRows)
                     {
+                        // User is valid, now update the last login
+                        mdr.Close(); // Close the reader before using it again
+
+                        string MyConnection2 = "Server=127.0.0.1;Database=library;username=root;password=";
+                        string Query = "UPDATE user_register SET LastLogin = @studentnum WHERE student_num = @studentnum";
+                        MySqlConnection MyConn2 = new MySqlConnection(MyConnection2);
+
+                        MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+                        MyCommand2.Parameters.AddWithValue("@studentnum", studentnumtxt.Text);
+                        MyConn2.Open();
+                        MyCommand2.ExecuteNonQuery();
+                        MyConn2.Close();
+
+                        MessageBox.Show("Login Successful!");
+                        this.Hide(); // Hide login form (or navigate to another form)
                     }
-                    MyConn2.Close();
-
-                    MessageBox.Show("Login Successful!");
-                    this.Hide();
-                    
-
-
+                    else
+                    {
+                        MessageBox.Show("Incorrect Login Information! Try again.");
+                    }
                 }
-                else
+                catch (Exception ex)
                 {
-
-                    MessageBox.Show("Incorrect Login Information! Try again.");
+                    MessageBox.Show(ex.Message);
                 }
-
-                connection.Close();
+                finally
+                {
+                    connection.Close();
+                }
             }
 
         }
